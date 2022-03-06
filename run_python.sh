9dc436f595a2e88d75940f46c681a5a67b8e1707
root <root@localhost.localdomain>
2022-03-06 11:26:17 +0800

    update the import

---
+++ run_python.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+cd /root/workspace/weiboSpider/
+/root/miniconda3/bin/python -m weibo_spider
--- weibo_spider/spider.py
+++ weibo_spider/spider.py
@@ -20,7 +20,7 @@ from . import config_util, datetime_util
 from .downloader import AvatarPictureDownloader
 from .parser import AlbumParser, IndexParser, PageParser, PhotoParser
 from .user import User
-import  datetime_util
+from . import  datetime_util
 
 FLAGS = flags.FLAGS
 
